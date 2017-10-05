143d84602028811f24bdd9bcfe768e9c6ee69341
miguel.gonzalez <miguel.gonzalez@mango.com>
2017-10-05 08:36:20 +0200

    Refactor: parametrized tests

--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,19 +1,23 @@
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class MarsRoverShould {
 
     @Test
-    public void turn_left() {
+    @Parameters({
+            "L, 0:0:W",
+            "LL, 0:0:S"
+    })
+    public void turn_left(String commands, String finalPosition) {
         MarsRover marsRover = new MarsRover(0,0,'N');
-        assertThat(marsRover.move("L"), is("0:0:W"));
+        assertThat(marsRover.move(commands), is(finalPosition));
     }
 
-    @Test
-    public void turn_left_twice() {
-        MarsRover marsRover = new MarsRover(0,0,'N');
-        assertThat(marsRover.move("LL"), is("0:0:S"));
-    }
 }
