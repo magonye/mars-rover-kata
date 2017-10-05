88b28ea53af68ded0f64282e6ae1b1c9c42a9cfa
miguel.gonzalez <miguel.gonzalez@mango.com>
2017-10-05 08:25:10 +0200

    Red: first test class

---
+++ src/main/java/MarsRover.java
@@ -0,0 +1,8 @@
+public class MarsRover {
+    public MarsRover(int x, int y, char direction) {
+    }
+
+    public String move(String command) {
+        return null;
+    }
+}
---
+++ src/test/java/MarsRoverShould.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MarsRoverShould {
+
+    @Test
+    public void turn_left() {
+        MarsRover marsRover = new MarsRover(0,0,'N');
+        assertThat(marsRover.move("L"), is("0:0:W"));
+    }
+}
