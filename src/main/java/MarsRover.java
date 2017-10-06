f2dba2a9a76e236b1ba452370c0cdec4d3219393
miguel.gonzalez <miguel.gonzalez@mango.com>
2017-10-06 08:44:55 +0200

    Refactor: Compass

--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -15,19 +15,38 @@ public class MarsRover {
         }
     }
 
+    public class Compass {
+        private Direction[] space = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
+
+        private int directionIndex = 0;
+
+        public Direction turn(char side){
+            if (side == 'R'){
+                directionIndex = (directionIndex + 1)%4;
+            }
+            if (side == 'L'){
+                directionIndex = directionIndex - 1;
+                if (directionIndex<0) directionIndex = 3;
+            }
+            return space[directionIndex];
+        }
+    }
+
     Direction direction;
+    Compass compass;
 
     public MarsRover(int x, int y, Direction direction) {
         this.direction = direction;
+        compass = new Compass();
     }
 
     public String move(String commands) {
         for(char command: commands.toCharArray()){
             if (command=='L') {
-                direction = turn_left();
+                direction = compass.turn('L');
             }
             if (command=='R') {
-                direction = turn_right();
+                direction = compass.turn('R');
             }
         }
         return "0:0:"+ direction.getValue();
