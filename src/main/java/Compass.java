e6d56054b813c4de6d1659cf995e23f1da32dcf9
miguel.gonzalez <miguel.gonzalez@mango.com>
2017-10-06 08:58:20 +0200

    Refactor: Extract Compass

---
+++ src/main/java/Compass.java
@@ -0,0 +1,29 @@
+public class Compass {
+    private Direction[] space = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
+
+    private int directionIndex = 0;
+
+    public Compass(Direction initialDirection) {
+        for (int i = 0; i < space.length; i++) {
+            if (space[i]==initialDirection){
+                directionIndex = i;
+                break;
+            }
+        }
+    }
+    
+    public void turn(char side){
+        if (side == 'R'){
+            directionIndex = (directionIndex + 1)%4;
+        }
+        if (side == 'L') {
+            directionIndex = directionIndex - 1;
+            if (directionIndex < 0)
+                directionIndex = 3;
+        }
+    }
+
+    public Direction getCurrentDirection(){
+        return space[directionIndex];
+    }
+}
--- src/main/java/MarsRover.java
+++ src/main/java/MarsRover.java
@@ -1,34 +1,14 @@
 public class MarsRover {
-
-    public class Compass {
-        private Direction[] space = {Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST};
-
-        private int directionIndex = 0;
-
-        public Direction turn(char side){
-            if (side == 'R'){
-                directionIndex = (directionIndex + 1)%4;
-            }
-            if (side == 'L'){
-                directionIndex = directionIndex - 1;
-                if (directionIndex<0) directionIndex = 3;
-            }
-            return space[directionIndex];
-        }
-    }
-
-    Direction direction;
     Compass compass;
 
     public MarsRover(int x, int y, Direction direction) {
-        this.direction = direction;
-        compass = new Compass();
+        compass = new Compass(direction);
     }
 
     public String move(String commands) {
         for(char command: commands.toCharArray()){
-            direction = compass.turn(command);
+            compass.turn(command);
         }
-        return "0:0:"+ direction.getValue();
+        return "0:0:"+ compass.getCurrentDirection().getValue();
     }
 }
